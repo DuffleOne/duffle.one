b978944f82877af10f364d8df1070c001ca23639
Laura Miller <git@duffle.one>
2024-06-20 00:26:02 +0100

    remove old public folder

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
-(cd social && ./compile.sh)
-aws s3 sync ./public s3://duffle.one --acl public-read --follow-symlinks --delete
+(cd home && ./compile.sh)
+aws s3 sync ./home/build s3://duffle.one --acl public-read --follow-symlinks --delete
+# aws s3 sync ./display s3://display.duffle.one --acl public-read --follow-symlinks --delete
--- social/compile.sh
+++
@@ -1,4 +0,0 @@
-rm -f ../public/*.html
-
-php -f index.php > ../public/index.html
-php -f events.php > ../public/events.html
