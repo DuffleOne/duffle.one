c919bad927999bc34d6c4677dc4a9105c88b54d5
Laura Miller <git@duffle.one>
2025-09-26 12:10:10 +0100

    huge infra update - add quotes

---
+++ vite.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	root: 'src',
+	appType: 'mpa',
+	esbuild: {
+		target: 'es2022',
+	},
+	build: {
+		target: 'es2022',
+		outDir: '../build',
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				index: 'src/index.html',
+				"404": 'src/404.html',
+			},
+		},
+	},
+	optimizeDeps: {
+		entries: ['src/index.html', 'src/404.html'],
+	},
+})
